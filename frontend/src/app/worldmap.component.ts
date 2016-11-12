a1d75fa69c4a90e8b7eb65c276803b53ab546b4a
Simon Fan <xmin547@gmail.com>
2016-11-12 11:55:20 -0500

    generated worldmap component

---
+++ frontend/src/app/worldmap.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+// declare var K: any;
+
+@Component({
+  selector: 'worldmap',
+  templateUrl: './worldmap.component.html',
+  styleUrls: ['./worldmap.component.css']
+})
+export class WorldmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
